3e42cf6da0ccc0fabe51e36422e5f1ca9a200ad9
kurtiz <papiliocurtis@gmail.com>
2023-04-30 11:49:13 +0000

    completed categories component :iphone:

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ![logo](https://links.papareact.com/wru)
 
+[//]: # ([![wakatime]&#40;https://wakatime.com/badge/user/9657174f-2430-4dfd-aaef-2b316eb71a36/project/4dad109f-d383-4b01-8ab7-3eaefd27da98.svg&#41;]&#40;https://wakatime.com/badge/user/9657174f-2430-4dfd-aaef-2b316eb71a36/project/4dad109f-d383-4b01-8ab7-3eaefd27da98&#41;)
+
 Food Delivery Mobile App built with React Native (Client Version)
 This is pretty much a recreation of **Deliveroo** or **Bolt Food** 
 or **Uber Eats**
--- components/CategoryCard.js
+++ components/CategoryCard.js
@@ -4,14 +4,17 @@ import {Image, Text, TouchableOpacity, View} from "react-native";
 const CategoryCard = ({imgUrl, title}) => {
 
     return (
-        <TouchableOpacity>
+        <TouchableOpacity className="relative mr-2">
             <Image
                 source={{
                     uri: imgUrl,
                 }}
-                className="h-20 w-20 mx-1 rounded"
+                className="h-20 w-20 rounded"
             />
-            <Text className="text-sm">{title}</Text>
+            <Text
+                className="font-bold absolute bottom-1 left-1 text-white">
+                {title}
+            </Text>
         </TouchableOpacity>
     );
 };

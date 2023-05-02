4d696b109c208db2968cefb34fe2b263da2bba72
kurtiz <papiliocurtis@gmail.com>
2023-05-02 10:31:03 +0000

    added featured row component :iphone:
    Added feature Row component and Restaurant Card

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,6 +9,7 @@ import {
 } from "react-native-heroicons/outline";
 import {SafeAreaView} from "react-native-safe-area-context";
 import Categories from "../components/Categories";
+import FeaturedRow from "../components/FeaturedRow";
 
 
 const HomeScreen = () => {
@@ -22,8 +23,9 @@ const HomeScreen = () => {
     }, []);
 
     return (
-        <SafeAreaView className="bg-white pt-3">
+        <SafeAreaView className="bg-gray-50 pt-3">
             {/* Header */}
+
             <View className="flex-row pb-3 item-center mx-4 space-x-2">
                 <Image
                     source={{
@@ -45,22 +47,42 @@ const HomeScreen = () => {
 
 
             <View className="flex-row items-center space-x-2 pb-2 mx-4">
-                <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+                <View className="flex-row flex-1 items-center space-x-2 bg-gray-200 p-3">
                     <SearchIcon color="gray" size={20}/>
                     <TextInput
                         placeholder="Find Restaurants"
                         keyboardType="default"
+                        className="flex-1"
                     />
                 </View>
                 <AdjustmentsVerticalIcon color={MainEnv.themeColor}/>
             </View>
             {/* End Search bar and adjustments */}
+                    {/* body */}
+                    <ScrollView>
+                        {/* Categories Row */}
+                        <Categories/>
+                        {/* End Categories Row */}
 
+                        {/* Features Row */}
+                        <FeaturedRow
+                            id="f11"
+                            title="Featured"
+                            description="Recommended based on your previous orders"
+                        />
 
-            {/* body */}
-            <ScrollView>
-                <Categories />
-            </ScrollView>
+                        <FeaturedRow
+                            id="f22"
+                            title="Yummy Discounts"
+                            description="Incredible Discounts you can't afford to miss"
+                        />
+                        <FeaturedRow
+                            id="f33"
+                            title="Near You"
+                            description="Order fast from the nearest restaurants"
+                        />
+
+                    </ScrollView>
 
         </SafeAreaView>
     )

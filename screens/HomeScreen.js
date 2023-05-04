c6c31c659dd3bc278f33dee07cdb199d7dab798c
kurtiz <papiliocurtis@gmail.com>
2023-05-04 09:05:25 +0000

    Connected Featured Rows to backend (sanity.io)

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import {View, Text, Image, TextInput, ScrollView} from 'react-native'
 import {useNavigation} from "@react-navigation/native";
-import {useLayoutEffect} from "react";
+import {useEffect, useLayoutEffect, useState} from "react";
 import MainEnv from "../main.env";
 import {
     ChevronDownIcon,
@@ -10,10 +10,15 @@ import {
 import {SafeAreaView} from "react-native-safe-area-context";
 import Categories from "../components/Categories";
 import FeaturedRow from "../components/FeaturedRow";
+import {getFeatured} from "../sanity.js";
 
 
 const HomeScreen = () => {
     const navigation = useNavigation(); // gives access to react navigation
+    const [
+        featuredCategories,
+        setFeaturedCategories
+    ] = useState([]); // state to store the featured
 
     // specify activities to take place as soon as this screen appears
     useLayoutEffect(() => {
@@ -22,8 +27,19 @@ const HomeScreen = () => {
         })
     }, []);
 
+    // hook for whenever the screen component loads
+    useEffect(() => {
+        //get the featured list from the backend
+        getFeatured()
+            .then(
+                data => setFeaturedCategories(data)
+            ).catch(
+            err => alert(err)
+        );
+    }, []);
+
     return (
-        <SafeAreaView className="bg-gray-50 pt-3">
+        <SafeAreaView className="bg-gray-50 pt-2 flex-1">
             {/* Header */}
 
             <View className="flex-row pb-3 item-center mx-4 space-x-2">
@@ -58,32 +74,54 @@ const HomeScreen = () => {
                 <AdjustmentsVerticalIcon color={MainEnv.themeColor}/>
             </View>
             {/* End Search bar and adjustments */}
-                    {/* body */}
-                    <ScrollView>
-                        {/* Categories Row */}
-                        <Categories/>
-                        {/* End Categories Row */}
 
-                        {/* Features Row */}
-                        <FeaturedRow
-                            id="f11"
-                            title="Featured"
-                            description="Recommended based on your previous orders"
-                        />
+            {/* body */}
+            <ScrollView>
+                {/* Categories Row */}
+                <Categories/>
+                {/* End Categories Row */}
 
+                {/* Featured Rows */}
+                {featuredCategories?.map((category, catIndex, row) => (
+                    // conditionally add a bottom margin to the last element of the map
+                    catIndex + 1 === row.length ?
                         <FeaturedRow
-                            id="f22"
-                            title="Yummy Discounts"
-                            description="Incredible Discounts you can't afford to miss"
-                        />
+                            key={category._id}
+                            id={category._id}
+                            title={category.name}
+                            classes="mb-2"
+                            description={category.short_description}
+                        /> :
                         <FeaturedRow
-                            id="f33"
-                            title="Near You"
-                            description="Order fast from the nearest restaurants"
-                        />
+                            key={category._id}
+                            id={category._id}
+                            title={category.name}
+                            classes="mb-2"
+                            description={category.short_description}
+                            />
+                ))}
 
+
+                {/*<FeaturedRow*/}
+                {/*    id="f11"*/}
+                {/*    title="Featured"*/}
+                {/*    description="Recommended based on your previous orders"*/}
+                {/*/>*/}
+
+                {/*<FeaturedRow*/}
+                {/*    id="f22"*/}
+                {/*    title="Yummy Discounts"*/}
+                {/*    description="Incredible Discounts you can't afford to miss"*/}
+                {/*/>*/}
+                {/*<FeaturedRow*/}
+                {/*    id="f33"*/}
+                {/*    classes="mb-2"*/}
+                {/*    title="Near You"*/}
+                {/*    description="Order fast from the nearest restaurants"*/}
+                {/*/>*/}
+                {/* End Featured Rows */}
                     </ScrollView>
-
+                {/* End body */}
         </SafeAreaView>
     )
 };

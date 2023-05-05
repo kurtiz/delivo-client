7372b2109d5e162b2794d10d2c844e3d32c4bdde
kurtiz <papiliocurtis@gmail.com>
2023-05-05 12:05:48 +0000

    Added basket component to Restaurant Screen :iphone:

--- features/basketSlice.js
+++ features/basketSlice.js
@@ -15,7 +15,7 @@ export const basketSlice = createSlice({
             const index = state.items.findIndex(item => item.id === action.payload.id);
             let copyOfBasket = [...state.items];
 
-            if(index >= 0){
+            if (index >= 0) {
                 copyOfBasket.splice(index, 1)
             } else {
                 alert(`Can't remove product with id of "${action.payload.id}" from the basket`)
@@ -36,4 +36,7 @@ export const selectBasketItems = (state) => state.basket.items;
 
 export const selectBasketItemsWithId = (state, id) => state.basket.items.filter(
     (item) => item.id === id);
+
+export const selectBasketTotal = (state) => state.basket.items.reduce(
+    (total, item) => total += item.price, 0);
 export default basketSlice.reducer
